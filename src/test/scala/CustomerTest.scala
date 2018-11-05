005e82dfa2ad1ce7e9273cf79e8a72febeab18a6
Cristian Dina <cristi.dina93@yahoo.com>
2018-11-05 21:09:20 +0200

    Step 2

--- src/test/scala/CustomerTest.scala
+++ src/test/scala/CustomerTest.scala
@@ -18,7 +18,42 @@ class CustomerTest extends FunSuite {
 
   test("Customer.checkOut - Happy Path") {
     val customer = new Customer
-    assert(customer.checkOut(List(Fruit.Apple, Fruit.Apple, Fruit.Orange, Fruit.Apple)) === "=> £2.05")
+    assert(customer.checkOut(List(Fruit.Apple, Fruit.Orange, Fruit.Orange, Fruit.Orange, Fruit.Apple)) === Fruit.Apple.price + Fruit.Orange.price * 2)
   }
+
+  test("Customer.checkOut - Oranges special offer for exact number of oranges for special offer") {
+    val customer = new Customer
+    val count = 3
+    assert(customer.checkOut(List.fill(count)(Fruit.Orange)) === getOrangesSpecialOffer(count))
+  }
+
+  test("Customer.checkOut - Oranges special offer and 1 extra orange") {
+    val customer = new Customer
+    val count = 4
+    assert(customer.checkOut(List.fill(count)(Fruit.Orange)) === getOrangesSpecialOffer(count))
+  }
+
+  test("Customer.checkOut - Oranges special offer and 2 extra orange") {
+    val customer = new Customer
+    val count = 5
+    assert(customer.checkOut(List.fill(count)(Fruit.Orange)) === getOrangesSpecialOffer(count))
+  }
+
+  test("Customer.checkOut - Apples special offer for exact number of apples for special offer") {
+    val customer = new Customer
+    val count = 2
+    assert(customer.checkOut(List.fill(count)(Fruit.Apple)) === getApplesSpecialOffer(count))
+  }
+
+  test("Customer.checkOut - Apples special offer and 1 extra apple") {
+    val customer = new Customer
+    val count = 3
+    assert(customer.checkOut(List.fill(count)(Fruit.Apple)) === getApplesSpecialOffer(count))
+  }
+
+  private def getOrangesSpecialOffer(count: Int): Int = Fruit.Orange.price * (count * 2 / 3 + count % 3)
+
+  private def getApplesSpecialOffer(count: Int): Int = Fruit.Apple.price * (count / 2 + count % 2)
 }
+
 
